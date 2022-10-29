732d6d58a4f260bb7b958241a99e7945e836a98d
Furkan Emre Aygün <feyup5076@gmail.com>
2022-10-29 20:44:34 +0300

    Support NameTag
    if name tags have custom name, tags now working with when player interacts mobs with nametag.

--- src/item/NameTag.php
+++ src/item/NameTag.php
@@ -4,23 +4,31 @@ declare(strict_types=1);
 
 namespace pocketmine\item;
 
-use pocketmine\block\Block;
-use pocketmine\entity\Entity;
-use pocketmine\entity\Location;
-use pocketmine\item\Item;
-use pocketmine\item\ItemUseResult;
-use pocketmine\math\AxisAlignedBB;
-use pocketmine\math\Vector3;
-use pocketmine\player\Player;
+use pocketmine\altay\Session;
 use pocketmine\entity\altay\AltayEntity;
+use pocketmine\entity\Entity;
+use pocketmine\player\Player;
 
 class NameTag extends Item{
 
-	public function onInteractAltayEntity(Player $player, AltayEntity $entity) : ItemUseResult{
-		$this->pop();
-			return parent::onInteractAltayEntity($player, $entity);
-
-		$this->pop();
-		return ItemUseResult::SUCCESS();
+	public function onInteractEntity(Player $player, Entity $entity) : ItemUseResult{
+		if ($entity instanceof AltayEntity) {
+			if ($player->getInventory()->getItemInHand() instanceof NameTag) {
+				if ($player->getInventory()->getItemInHand()->hasCustomName()) {
+					$entity->setNameTag($player->getInventory()->getItemInHand()->getCustomName());
+					Session::playSound($player, "note.bell");
+					if($player->hasFiniteResources()) {
+						$item = $player->getInventory()->getItemInHand();
+						$item->pop(1);
+						$player->getInventory()->setItemInHand($item);
+					}
+					return ItemUseResult::SUCCESS();
+				}
+			}
+		}
+		return ItemUseResult::FAIL();
+	}
+	public function getCooldownTicks() : int{
+		return 20;
 	}
 }
